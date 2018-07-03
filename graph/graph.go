9955dc9bd533dd2753eb616aceef8e394510515f
souppower <ryo.kubota@globis.co.jp>
2018-07-03 09:19:06 +0900

    first commit

---
+++ graph/graph.go
@@ -0,0 +1,43 @@
+//go:generate gorunpkg github.com/souppower/gqlgen -typemap types.json -schema ../schema.graphql
+
+package graph
+
+import (
+	context "context"
+	"fmt"
+	"math/rand"
+)
+
+type User struct {
+	ID   string
+	Name string
+}
+
+type Todo struct {
+	ID     string
+	Text   string
+	Done   bool
+	UserID string
+}
+
+type MyApp struct {
+	todos []Todo
+}
+
+func (a *MyApp) Query_todos(ctx context.Context) ([]Todo, error) {
+	return a.todos, nil
+}
+
+func (a *MyApp) Mutation_createTodo(ctx context.Context, input NewTodo) (Todo, error) {
+	todo := Todo{
+		Text:   input.Text,
+		ID:     fmt.Sprintf("T%d", rand.Int()),
+		UserID: input.UserId,
+	}
+	a.todos = append(a.todos, todo)
+	return todo, nil
+}
+
+func (a *MyApp) Todo_user(ctx context.Context, it *Todo) (User, error) {
+	return User{ID: it.UserID, Name: "user " + it.UserID}, nil
+}
